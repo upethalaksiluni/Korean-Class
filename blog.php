d3b5896133780a2374a80ab0dbdf8bb8cae8ebc6
vishwaranmina <vishwaranmina25@gmail.com>
2025-06-13 11:31:20 +0530

    all pages structure is commited

---
+++ blog.php
@@ -0,0 +1,334 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="apple-touch-startup-image" href="https://www.wepora.com/asset/img/wepora-logo.png">
+    <link rel="icon" type="image/x-icon" href="https://www.wepora.com/asset/img/wepora-logo.png">
+    <meta name="description"
+        content="Wepora is a best Graphics, software and Web Development company  and provides all IT solutions to their client. In india.." />
+    <meta name="Keywords"
+        content="website design | website development | website logo  |  website hosting  | logo design| logo design ideas  | SEO | android |  best software company in india | cheapest | graphic design | Shrikant Kushwaha">
+    <meta name="author" content="contain by Wepora team">
+    <meta name="copyright" content="Copyright © 2020 Wepora" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        crossorigin="anonymous"></script>
+    <title>blog</title>
+    <link rel="stylesheet" href="asset/css/style.css">
+</head>
+
+<body>
+    <!--content start-->
+
+    <!-- Header Start -->
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <img class="navimg img-fluid" src="asset/img/logo/main-logo.png" alt="class-logo">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse navmenu" id="navbarSupportedContent">
+                    <ul id="navbar"
+                        class="navbar-nav text-uppercase align-items-start align-items-lg-center justify-content-end navbar-collapse ms-auto">
+                        <li class="nav-item">
+                            <a class="nav-link text-white me-4 active" href="index.html">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white me-4" href="about.html">About us</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white me-4" href="Books.html">Books</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white me-4" href="Schedules.html">Class Schedules</a>
+                        </li>
+                         <li class="nav-item">
+                            <a class="nav-link text-white me-4" href="blog.php">Blogs</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white me-4" href="contact.html">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <!-- Header End -->
+
+</header>
+        <main>
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 mt-3">
+            <i>
+                <h1 class="text-light mb-3 text-center backgroundblue blodfont">Korean Blog</h1>
+            </i>
+            <div class="card border-0 cardBoxShadow">
+                <img class="card-img-top" src="./hq720-2.jpg" alt="Title" />
+                <div class="card-body">
+    <blockquote class="blockquote mb-0">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+      <footer class="blockquote-footer bg-light text-dark">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+    </blockquote>
+    <p class="mt-3">
+        <?php $blog="Information is at the core of human progress. It’s why we’ve focused for more than 26 years on our mission to organize the world’s information and make it accessible and useful. And it’s why we continue to push the frontiers of AI to organize that information across every input and make it accessible via any output, so that it can be truly useful for you.
+That was our vision when we introduced Gemini 1.0 last December. The first model built to be natively multimodal, Gemini 1.0 and 1.5 drove big advances with multimodality and long context to understand information across text, video, images, audio and code, and process a lot more of it.
+Now millions of developers are building with Gemini. And it’s helping us reimagine all of our products — including all 7 of them with 2 billion users — and to create new ones. NotebookLM is a great example of what multimodality and long context can enable for people, and why it’s loved by so many.
+Over the last year, we have been investing in developing more agentic models, meaning they can understand more about the world around you, think multiple steps ahead, and take action on your behalf, with your supervision.
+Today we’re excited to launch our next era of models built for this new agentic era: introducing Gemini 2.0, our most capable model yet. With new advances in multimodality — like native image and audio output — and native tool use, it will enable us to build new AI agents that bring us closer to our vision of a universal assistant.
+We’re getting 2.0 into the hands of developers and trusted testers today. And we’re working quickly to get it into our products, leading with Gemini and Search. Starting today our Gemini 2.0 Flash experimental model will be available to all Gemini users. We're also launching a new feature called Deep Research, which uses advanced reasoning and long context capabilities to act as a research assistant, exploring complex topics and compiling reports on your behalf. It's available in Gemini Advanced today, and you can learn more on our website."; 
+
+echo nl2br($blog);
+?>
+   
+
+</p>
+  </div>
+  
+            </div>
+            <div class="mt-5">
+        <div class="card">
+  <div class="card-header">
+    Quote
+  </div>
+  <div class="card-body">
+    <blockquote class="blockquote mb-0">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+      <footer class="blockquote-footer bg-light text-dark">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+    </blockquote>
+  </div>
+</div>
+    </div>
+        </div>
+        <div class="col-md-4 mt-3">
+<div class="mb-4 ">
+    <h2 class="text-light backgroundblue text-center pb-2">Recent Blogs</h2>
+</div>
+<div class="blog">
+    <div class="card mb-3 border-0">
+        <h5 class="card-title ms-3 text-uppercase  fw-bold text-danger" >study Korean With me</h5>
+  <img class="card-img-top mx-3" src="./maxresdefault.jpg" alt="Card image cap">
+  <div class="card-body">
+    
+    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    <p class="card-text mb-3"><small class="text-muted">Last updated 3 mins ago</small></p>
+    <hr>
+  </div>
+</div>
+<div class="card border-0">
+    <h5 class="card-title ms-3 text-uppercase  fw-bold text-danger" >study Korean With me</h5>
+    <img class="card-img-bottom mx-3" src="./hq720.jpg" alt="Card image cap">
+  <div class="card-body">    
+    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    <p class="card-text mb-3"><small class="text-muted">Last updated 3 mins ago</small></p>
+    <hr>
+  </div>
+  
+</div>
+<div class="card border-0">
+    <h5 class="card-title ms-3 text-uppercase  fw-bold text-danger" >study Korean With me</h5>
+    <img class="card-img-bottom mx-3" src="./hq720.jpg" alt="Card image cap">
+  <div class="card-body">    
+    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    <p class="card-text mb-3"><small class="text-muted">Last updated 3 mins ago</small></p>
+    <hr>
+  </div>
+  
+</div>
+</div>
+        </div>
+    </div>
+</div>
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="row row-cols-1 row-cols-md-4 g-4">
+  <div class="col">
+    <div class="card">
+      <img src="./hq720-2.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="./hq720-2.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="./hq720-2.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="./hq720-2.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+</div>
+        </div>
+    </div>
+</div>
+        </main>
+
+    <!-- footer -->
+
+    <footer class="page-footer pt-5 mt-3">
+        <div class="container">
+            <div class="row justify-content-center text-center text-lg-start">
+                <!-- Column 1: Logo and Description -->
+                <div class="col-12 col-sm-10 col-md-8 col-lg-3 mb-4">
+                    <div class="footer-content">
+                        <a class="d-inline-block mb-3" href="#">
+                            <img src="asset/img/logo/main-logo.png" alt="class-logo" class="img-fluid"
+                                style="max-width: 100px;">
+                        </a>
+                        <h3 class="h5 mb-3">Korean Class</h3>
+                        <p class="text-light mb-4">
+                            At KoreanClass.lk, we are dedicated to providing top-quality Korean
+                            language education in Sri Lanka.
+                        </p>
+                    </div>
+                </div>
+
+                <!-- Column 2: Quick Links -->
+                <div class="col-12 col-sm-10 col-md-8 col-lg-3 mb-4">
+                    <div class="footer-content">
+                        <h3 class="h5 mb-3 text-decoration-underline ">Quick Links</h3>
+                        <ul class="list-unstyled footer-links">
+                            <li class="mb-2"><a href="index.html" class="text-light text-decoration-none">Home</a></li>
+                            <li class="mb-2"><a href="About.html" class="text-light text-decoration-none">About us</a>
+                            </li>
+                            <li class="mb-2"><a href="Books.html" class="text-light text-decoration-none">Books</a></li>
+                            <li class="mb-2"><a href="Schedules.html" class="text-light text-decoration-none">Class
+                                    Schedules</a></li>
+                            <li class="mb-2"><a href="Contact.html" class="text-light text-decoration-none">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <!-- Column 3: Contact Information -->
+                <div class="col-12 col-sm-10 col-md-8 col-lg-3 mb-4">
+                    <div class="footer-content">
+                        <h3 class="h5 mb-3 text-decoration-underline ">Contact Us</h3>
+                        <ul class="list-unstyled footer-links">
+                            <li
+                                class="d-flex align-items-center mb-3 justify-content-center justify-content-lg-start text-white">
+                                <i class="bi bi-geo-alt-fill me-2 text-white"></i>
+                                <span class="map-link">
+                                    <a href="https://maps.app.goo.gl/TUadGopccNf45uGb7" class="text-white"
+                                        style="text-decoration: none;">Address Here, Mathugama</a>
+                                </span>
+                            </li>
+                            <li class="d-flex align-items-center mb-3 justify-content-center justify-content-lg-start">
+                                <i class="bi bi-telephone-fill me-2 text-primary"></i>
+                                <span class="text-light">+94 778 050 666</span>
+                            </li>
+                            <li class="d-flex align-items-center mb-3 justify-content-center justify-content-lg-start">
+                                <i class="bi bi-envelope-fill me-2 text-primary"></i>
+                                <span class="text-light">info@koreanclass.lk</span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <!-- Column 4: Social Links -->
+                <div class="col-12 col-sm-10 col-md-8 col-lg-3 mb-4">
+                    <div class="footer-content">
+                        <h3 class="h5 mb-3 text-decoration-underline ">Follow Us</h3>
+                        <div class="social-links d-flex flex-column align-items-center align-items-lg-start">
+                            <a href="https://www.facebook.com/koreanclasslk"
+                                class="footer-social-link d-flex align-items-center mb-2">
+                                <i class="fa-brands fa-facebook me-3 social-icon"></i>
+                                <span>Facebook</span>
+                            </a>
+                            <a href="https://www.youtube.com/@samanthajayawardanakorea"
+                                class="footer-social-link d-flex align-items-center mb-2">
+                                <i class="fa-brands fa-youtube me-3 social-icon"></i>
+                                <span>Youtube</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- footer -->
+
+
+
+
+    <!--content end-->
+    <!-- JavaScript -->
+    <script src="asset/js/script.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js"
+        integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js"
+        integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG"
+        crossorigin="anonymous"></script>
+<!-- Code injected by live-server -->
+<script>
+	// <![CDATA[  <-- For SVG support
+	if ('WebSocket' in window) {
+		(function () {
+			function refreshCSS() {
+				var sheets = [].slice.call(document.getElementsByTagName("link"));
+				var head = document.getElementsByTagName("head")[0];
+				for (var i = 0; i < sheets.length; ++i) {
+					var elem = sheets[i];
+					var parent = elem.parentElement || head;
+					parent.removeChild(elem);
+					var rel = elem.rel;
+					if (elem.href && typeof rel != "string" || rel.length == 0 || rel.toLowerCase() == "stylesheet") {
+						var url = elem.href.replace(/(&|\?)_cacheOverride=\d+/, '');
+						elem.href = url + (url.indexOf('?') >= 0 ? '&' : '?') + '_cacheOverride=' + (new Date().valueOf());
+					}
+					parent.appendChild(elem);
+				}
+			}
+			var protocol = window.location.protocol === 'http:' ? 'ws://' : 'wss://';
+			var address = protocol + window.location.host + window.location.pathname + '/ws';
+			var socket = new WebSocket(address);
+			socket.onmessage = function (msg) {
+				if (msg.data == 'reload') window.location.reload();
+				else if (msg.data == 'refreshcss') refreshCSS();
+			};
+			if (sessionStorage && !sessionStorage.getItem('IsThisFirstTime_Log_From_LiveServer')) {
+				console.log('Live reload enabled.');
+				sessionStorage.setItem('IsThisFirstTime_Log_From_LiveServer', true);
+			}
+		})();
+	}
+	else {
+		console.error('Upgrade your browser. This Browser is NOT supported WebSocket for Live-Reloading.');
+	}
+	// ]]>
+</script>
+</body>
+
+</html>
